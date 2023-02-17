e9500914ec68221905c2c5a89b5fb788a688d1eb
salvadiaz <salvador.diaz@bluealba.com>
2023-02-16 21:41:00 -0300

    microservices templates added

---
+++ api-customer/src/main/java/com/dh/g5/apicustomer/ApiCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.dh.g5.apicustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCustomerApplication.class, args);
+	}
+
+}
