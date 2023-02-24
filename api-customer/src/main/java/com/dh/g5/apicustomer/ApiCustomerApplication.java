057e120c9ffce1d0c45b48b91235575c9a255eb5
Camilo  Martinez <camilo_2510@hotmail.com>
2023-02-23 20:34:54 -0500

    implementing config server

--- api-customer/src/main/java/com/dh/g5/apicustomer/ApiCustomerApplication.java
+++ api-customer/src/main/java/com/dh/g5/apicustomer/ApiCustomerApplication.java
@@ -2,7 +2,9 @@ package com.dh.g5.apicustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiCustomerApplication {
 
---
+++ config-server/src/test/java/com/dh202302/g5/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dh202302.g5.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+
+
+}
