55585a533fd494d92ea38f1fa0340a49317a2827
Camilo  Martinez <camilo_2510@hotmail.com>
2023-02-28 13:36:53 -0500

    adjusting poms

--- api-customer/src/main/java/com/dh/g5/apicustomer/repository/CustomerRepository.java
+++ api-customer/src/main/java/com/dh/g5/apicustomer/repository/CustomerRepository.java
@@ -4,11 +4,13 @@ import com.dh.g5.apicustomer.models.Customer;
 import com.dh.g5.apicustomer.models.DocType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    @Transactional
     List<Customer> findByDocTypeOrDocumentNumber(DocType docType, String documentNumber);
 }
