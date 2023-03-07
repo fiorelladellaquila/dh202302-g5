6a73a90ac5e8574686f3f02e412f7b6ce9937462
Juan Carlos Gutierrez <juancgutierrezh@gmail.com>
2023-03-06 21:24:00 -0500

    getter y setter en la clase

--- api-customer/src/main/java/com/dh/g5/apicustomer/dto/CustomerInput.java
+++ api-customer/src/main/java/com/dh/g5/apicustomer/dto/CustomerInput.java
@@ -2,6 +2,7 @@ package com.dh.g5.apicustomer.dto;
 
 import com.dh.g5.apicustomer.models.DocType;
 import com.dh.g5.apicustomer.models.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -34,6 +35,7 @@ public class CustomerInput {
 
     @NotNull
     @NotBlank
+    //@JsonFormat(pattern="yyyy-MM-dd")
     private String birthDate;
 
 }
