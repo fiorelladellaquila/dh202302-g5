4c5a610e794a634efb37bfcba6b7020737c00444
Juan Carlos Gutierrez <juancgutierrezh@gmail.com>
2023-03-21 20:42:35 -0500

    adding post to controller

--- api-card/src/main/java/com/dh2023g5/apicard/client/MarginsClient.java
+++ api-card/src/main/java/com/dh2023g5/apicard/client/MarginsClient.java
@@ -11,29 +11,40 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @FeignClient(name="api-margins")
 @LoadBalancerClient(value="api-margins", configuration= LoadBalancerConfiguration.class)
 public interface MarginsClient {
 
     @GetMapping("/calculate/{documentType}/{documentValue}")
-    public Calification calculateCalification(@PathVariable String documentType, @PathVariable String documentValue){
-        return calculateMargin.calculate(documentType,documentValue);
-    }
+    public Calification calculateCalification(@PathVariable String documentType, @PathVariable String documentValue);
+
 
 
     @Getter
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
-    class CustomerDTO {
+    class Calification {
 
-        private String name;
-        private String documentNumber;
-        private String surname;
-        private String gender;
-        private LocalDate birthDate;
+        private String documentType;
+        private String documentValue;
+        private BigDecimal totalMargin;
+        private BigDecimal totalMarginAdditional;
+        private List<Sublimit> sublimits;
+        @Getter
+        @Setter
+        public static class Sublimit{
+            private Concept concept;
+            private BigDecimal totalMargin;
+        }
+
+        public enum Concept {
+            CARD,LOAN,CHECK
+        }
 
     }
 
---
+++ api-card/src/main/java/com/dh2023g5/apicard/controller/CardController.java
@@ -0,0 +1,17 @@
+package com.dh2023g5.apicard.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/card")
+public class CardController {
+
+public ResponseEntity<?> createCard()
+
+
+
+
+
+}
