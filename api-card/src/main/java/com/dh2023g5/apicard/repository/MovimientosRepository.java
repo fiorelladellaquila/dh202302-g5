d1ce9f86164bb27040ead5257ff3a551823a83c6
Juan Carlos Gutierrez <juancgutierrezh@gmail.com>
2023-03-26 18:47:53 -0500

    repository Movimientos correjido

--- api-card/src/main/java/com/dh2023g5/apicard/repository/MovimientosRepository.java
+++ api-card/src/main/java/com/dh2023g5/apicard/repository/MovimientosRepository.java
@@ -1,8 +1,10 @@
 package com.dh2023g5.apicard.repository;
 
+import com.dh2023g5.apicard.model.MovimientosTarjetaCredito;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovimientosRepository extends MongoRepository<MovimientosRepository,String> {
+public interface MovimientosRepository extends MongoRepository<MovimientosTarjetaCredito,String> {
+
 }
